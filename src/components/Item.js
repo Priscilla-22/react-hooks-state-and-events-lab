dca5d111de21cd3dd068e8f1502a536c076f9abb
Priscilla-22 <priscillamw123@gmail.com>
2024-02-01 19:09:42 +0300

    feat: Implement adding/removing items to virtual cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Item({ name, category }) {
+  const [addCart, setAddCart] = useState(false);
+
+  const handleAddCart = () => setAddCart(!addCart);
   return (
-    <li className="">
+    <li className={addCart ? 'add cart' : ''}>
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span className='category'>{category}</span>
+      <button className='add' onClick={handleAddCart}>
+        {addCart ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
